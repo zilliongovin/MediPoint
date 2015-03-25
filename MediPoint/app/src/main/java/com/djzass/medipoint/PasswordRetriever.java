4db753bc97b5af8e43fbf4f495f9fe8f809a8b2f
ShreyasMundhra <shreyass001@e.ntu.edu.sg>
2015-03-25 15:03:50 +0800

    Extends Activity

--- MediPoint/app/src/main/java/com/djzass/medipoint/PasswordRetriever.java
+++ MediPoint/app/src/main/java/com/djzass/medipoint/PasswordRetriever.java
@@ -1,12 +1,13 @@
 package com.djzass.medipoint;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class PasswordRetriever extends ActionBarActivity {
+public class PasswordRetriever extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
