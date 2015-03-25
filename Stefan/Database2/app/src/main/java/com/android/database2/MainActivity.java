14215142586bc978eef3251fb26e6f175633316a
Stefan0010 <dphoenix32@yahoo.com>
2015-03-25 14:02:58 -0700

    add my files
    like the titles says

---
+++ Stefan/Database2/app/src/main/java/com/android/database2/MainActivity.java
@@ -0,0 +1,104 @@
+package com.android.database2;
+
+/**
+ * Created by Z480 on 3/23/2015.
+ */
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        quantityBox =
+                (EditText) findViewById(R.id.productQuantity);
+    }
+
+    public void newProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        int quantity =
+                Integer.parseInt(quantityBox.getText().toString());
+
+        Product product =
+                new Product(productBox.getText().toString(), quantity);
+
+        dbHandler.addProduct(product);
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        Product product =
+                dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null,
+                null, 1);
+
+        boolean result = dbHandler.deleteProduct(
+                productBox.getText().toString());
+
+        if (result) {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        } else
+            idView.setText("No Match Found");
+    }
+
+    public void viewProduct(View view){
+        Intent intent = new Intent(this,AnotherActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
