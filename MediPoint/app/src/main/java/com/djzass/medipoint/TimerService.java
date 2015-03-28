880654349840b44638a67505d668b89e825ebca7
Deka <deka108@gmail.com>
2015-03-28 16:35:23 +0800

    TimerService to be overwritten

--- MediPoint/app/src/main/java/com/djzass/medipoint/TimerService.java
+++ MediPoint/app/src/main/java/com/djzass/medipoint/TimerService.java
@@ -2,8 +2,10 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
 
 public class TimerService extends Service {
@@ -53,9 +55,6 @@ public class TimerService extends Service {
         }
     };
 }
-
-
-
 
 /*
 ADD THIS TO MANIFEST FOR SERVICE!
