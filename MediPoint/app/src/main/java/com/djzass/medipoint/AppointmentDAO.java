80c9b4b9411329b4a239eead27d05bf7166e97ff
VotBear <vot1_bear@yahoo.com>
2015-03-30 20:01:30 +0800

    DAO fix

--- MediPoint/app/src/main/java/com/djzass/medipoint/AppointmentDAO.java
+++ MediPoint/app/src/main/java/com/djzass/medipoint/AppointmentDAO.java
@@ -27,14 +27,16 @@ public class AppointmentDAO extends DbDAO{
 
     public AppointmentDAO(Context context) throws SQLException {
         super(context);
+        initializeDAO();
     }
 
     public long insertAppointment(Appointment appointment) {
         ContentValues values = new ContentValues();
+        values.put(DbContract.AppointmentEntry.COLUMN_NAME_APPOINTMENT_ID, getAppointmentCount());
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appointment.getClinicId());
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_PATIENT_ID, appointment.getPatientId());
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_DOCTOR_ID, appointment.getDoctorId());
-        values.put(DbContract.AppointmentEntry.COLUMN_NAME_DATE_TIME, String.valueOf(appointment));
+        values.put(DbContract.AppointmentEntry.COLUMN_NAME_DATE_TIME, String.valueOf(appointment.getDate()));
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_SERVICE_ID, appointment.getService());
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_SPECIALTY_ID,appointment.getSpecialtyId());
         values.put(DbContract.AppointmentEntry.COLUMN_NAME_PREAPPOINTMENT_ACTIONS, appointment.getPreAppointmentActions());
@@ -107,9 +109,6 @@ public class AppointmentDAO extends DbDAO{
         String whereclause = DbContract.AppointmentEntry.COLUMN_NAME_DOCTOR_ID + " = " + did;
         return getAppointments(whereclause);
     }
-
-
-
     /*
         UPDATE
         returns the number of rows affected by the update
@@ -154,53 +153,11 @@ public class AppointmentDAO extends DbDAO{
         }
     }
 
+    public int getAppointmentCount(){
+        return getAllAppointments().size();
+    }
+    private void initializeDAO(){
+        if (getAllAppointments().size()==0){
+        }
+    }
 }
-
-    /*
-    //Conflicting code??
-    public long save(Appointment appointment) {
-        ContentValues values = new ContentValues();
-        values.put(DbHelper.NAME_COLUMN, appointment.getName());
-
-        return database.insert(DbHelper.DEPARTMENT_TABLE, null, values);
-    }
-
-    public long update(Appointment appointment) {
-        ContentValues values = new ContentValues();
-        values.put(DbHelper.NAME_COLUMN, appointment.getName());
-
-        long result = database.update(DbHelper.DEPARTMENT_TABLE, values,
-                WHERE_ID_EQUALS,
-                new String[] { String.valueOf(department.getId()) });
-        Log.d("Update Result:", "=" + result);
-        return result;
-
-    }
-
-    public int deleteDept(Department department) {
-        return database.delete(DataBaseHelper.DEPARTMENT_TABLE,
-                WHERE_ID_EQUALS, new String[] { department.getId() + "" });
-    }
-
-
-    public void loadAppointments() {
-
-        Appointment app1 = new Appointment();
-        Appointment app2 = new Appointment();
-        Appointment app3 = new Appointment();
-
-        ArrayList<Appointment> appointments = new ArrayList<Appointment>();
-        appointments.add(app1);
-        appointments.add(app2);
-        appointments.add(app3);
-
-        for (Appointment appt : appointments) {
-            ContentValues values = new ContentValues();
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            values.put(DbContract.AppointmentEntry.COLUMN_NAME_CLINIC_ID, appt.getClinicId());
-            database.insert(DbContract.AppointmentEntry.TABLE_NAME, null, values);
-        }*/
