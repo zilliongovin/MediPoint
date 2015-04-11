a1b0239ea45aa0ec78bcadc8e3196eed2639d747
Stefan0010 <dphoenix32@yahoo.com>
2015-04-10 22:28:00 -0700

    left behind

--- MediPoint/app/src/main/java/com/djzass/medipoint/entity/Specialty.java
+++ MediPoint/app/src/main/java/com/djzass/medipoint/entity/Specialty.java
@@ -1,9 +1,12 @@
 package com.djzass.medipoint.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Deka on 26/3/2015.
  */
-public class Specialty {
+public class Specialty implements Parcelable{
     private int id;
     private String name;
 
@@ -35,4 +38,36 @@ public class Specialty {
     public void setId(int id) {
         this.id = id;
     }
+
+    public Specialty(Parcel in){
+        readFromParcel(in);
+    }
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel desc, int flags) {
+        desc.writeInt(this.id);
+        desc.writeString(this.name);
+    }
+
+    public static final Parcelable.Creator<Specialty> CREATOR
+            = new Parcelable.Creator<Specialty>() {
+        public Specialty createFromParcel(Parcel in) {
+            return new Specialty(in);
+        }
+
+        public Specialty[] newArray(int size) {
+            return new Specialty[size];
+        }
+    };
+
+    public void readFromParcel(Parcel in) {
+        this.name = in.readString();
+        this.id = in.readInt();
+    }
+
+
 }
