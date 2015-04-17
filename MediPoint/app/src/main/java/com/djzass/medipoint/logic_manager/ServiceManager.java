7405b075a1a5d2d8f5c59b04484dd69cb20d9f06
Zillion-Govin <zillion.govin@gmail.com>
2015-04-18 00:20:21 +0800

    Service Manager javaDoc

--- MediPoint/app/src/main/java/com/djzass/medipoint/logic_manager/ServiceManager.java
+++ MediPoint/app/src/main/java/com/djzass/medipoint/logic_manager/ServiceManager.java
@@ -40,33 +40,69 @@ public class ServiceManager {
             e.printStackTrace();
         }
     }
+
+    /**
+     * get a List of a specific Service
+     * @param context Interface to global information about an application environment
+     * @return List containing Service object
+     */
     public List<Service> getServices(Context context){
         updateServiceDao(context);
         return serviceDao.getAllServices();
     }
 
+    /**
+     * get Service using the id
+     * @param serviceid int object containing the Id of the Service
+     * @param context Interface to global information about an application environment
+     * @return List of Service object
+     */
     public List<Service> getServicesByID(int serviceid,Context context) {
         updateServiceDao(context);
         return serviceDao.getServicesByID(serviceid);
     }
 
+    /**
+     * get Service using the specialtyId
+     * @param specialtyId int object containing the Id of the specialty
+     * @param context Interface to global information about an application environment
+     * @return List of Service object
+     */
     public List<Service> getServicesBySpecialtyID(int specialtyId,Context context) {
         updateServiceDao(context);
         return serviceDao.getServicesBySpecialtyID(specialtyId);
     }
 
+    /**
+     * get Service name using the id
+     * @param serviceid int object containing the Id of the Service
+     * @param context Interface to global information about an application environment
+     * @return String object containing Service name
+     */
     public String getServiceNameByID(int serviceid,Context context) {
         updateServiceDao(context);
         List<Service> temp = serviceDao.getServicesByID(serviceid);
         return temp.get(0).getName();
     }
 
+    /**
+     * get Service duration using the id
+     * @param serviceid int object containing the Id of the Service
+     * @param context Interface to global information about an application environment
+     * @return int object containing Service duration
+     */
     public int getServiceDurationbyID(int serviceid, Context context){
         updateServiceDao(context);
         List<Service> temp = serviceDao.getServicesByID(serviceid);
         return temp.get(0).getDuration();
     }
 
+    /**
+     * get Service preappointment action using the id
+     * @param serviceid int object containing the Id of the Service
+     * @param context Interface to global information about an application environment
+     * @return String object containing preappointment action for the Service
+     */
     public String getServicePreappbyID(int serviceid, Context context){
         updateServiceDao(context);
         List<Service> temp = serviceDao.getServicesByID(serviceid);
